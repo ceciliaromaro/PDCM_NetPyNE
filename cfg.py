f5af67ec380c101121aeb122a7cdc1b4a12a743e
salvadord <salvadordura@gmail.com>
2020-12-23 11:47:23 -0500

    trials_test

--- cfg.py
+++ cfg.py
@@ -21,7 +21,7 @@ cfg = specs.SimConfig() # object of class SimConfig to store simulation configur
 
 #cfg.seeds['stim']=6
 cfg.seeds['stim'] = 0
-cfg.duration = 60*1e3 #6*1e2   # Duration of the simulation, in ms
+cfg.duration = 0.1 #60*1e3 #6*1e2   # Duration of the simulation, in ms
 cfg.dt = 0.025          # Internal integration timestep to use
 cfg.verbose = 0     # Show detailed messages
 #cfg.seeds['m'] = 123
@@ -47,7 +47,7 @@ cfg.createPyStruct = False      # save memory by not saving py data structure fo
 # DC=False ; TH=True;  Balanced=True   => Figure 10A. But I want a partial reproduce so I guess Figure 10C is not necessary
 
 # Size of Network. Adjust this constants, please!
-cfg.ScaleFactor = 1.0 # 1.0 = 80.000 
+cfg.ScaleFactor = 0.1 #1.0 # 1.0 = 80.000 
 
 # External input DC or Poisson
 cfg.DC = False #True = DC // False = Poisson
@@ -84,7 +84,7 @@ cfg.analysis['plotRaster']={'include': [], 'timeRange': [100,600], 'popRates' : 
 
 # statistics plot (include update in netParams.py)
 cfg.analysis['plotSpikeStats'] = {'include' : [], 'stats' : ['rate','isicv'], 'legendLabels':['L6i', 'L6e', 'L5i', 'L5e', 'L4i', 'L4e','L2i', 'L2e'],
-	'timeRange' : [100,cfg.duration], 'fontSize': 16, 'figSize': (6,9),'showFig':False, 'saveFig': True, 'saveData':True}
+	'timeRange' : [100,cfg.duration], 'fontSize': 16, 'figSize': (6,9),'showFig': False, 'saveFig': True, 'saveData': True}
 
 # plot traces
 #cfg.recordTraces = {'m': {'var': 'm', 'conds':{'pop': ['L2e', 'L2i']}}}
